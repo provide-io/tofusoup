7b81f24251951de4bf6712303ddd527a48bb7edb
Tim <code@tim.life>
2025-12-02 20:00:00 -0800

    chore(config): update project configuration and dependencies
    Also:
    - scaffold(stir): introduce core Stir runtime and CLI infrastructure
    - interface(registry): wire registry backends and search engine
    - qualify(testing): add test coverage for core modules

---
+++ src/tofusoup/harness/proto/__init__.py
@@ -0,0 +1,7 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 2025 provide.io llc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+# 🥣🔬🔚
---
+++ src/tofusoup/harness/proto/kv/__init__.py
@@ -0,0 +1,12 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 2025 provide.io llc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+from . import kv_pb2, kv_pb2_grpc
+from .kv_protocol import KVProtocol
+
+__all__ = ["KVProtocol", "kv_pb2", "kv_pb2_grpc"]
+
+# 🥣🔬🔚
