ef0e80e82d897955ed4c32f8ddd515ce6a643c4b
Tim <code@tim.life>
2025-12-02 16:52:04 -0800

    🔼⚙️ [skip ci] auto-commit

--- tests/browser/test_cli.py
+++ tests/browser/test_cli.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-"""TODO: Add module docstring."""
 
 from provide.testkit import isolated_cli_runner
 import pytest
--- tests/browser/ui/widgets/test_detail_view.py
+++ tests/browser/ui/widgets/test_detail_view.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-"""TODO: Add module docstring."""
 
 import pytest
 from textual.app import ComposeResult
