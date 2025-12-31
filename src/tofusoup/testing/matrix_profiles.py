b314c9cd55db3f19ba4e0a1c1a69bfef261fb4bd
Tim <code@tim.life>
2025-12-30 18:59:52 -0800

    🔼⚙️ [skip ci] auto-commit

--- src/tofusoup/testing/matrix_profiles.py
+++ src/tofusoup/testing/matrix_profiles.py
@@ -30,7 +30,7 @@ except ImportError:
     WORKENV_AVAILABLE = False
     WorkenvConfig = None
     get_tool_manager = None
-    create_workenv_config_with_soup = None
+    create_workenv_config_with_soup = None  # type: ignore[assignment]
 
 console = Console()
 
