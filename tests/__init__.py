a12ecdadd72d5d8d33f600a6ad3b99bb8feca1ba
Tim <code@tim.life>
2026-01-10 20:00:00 -0800

    chore(spdx): add SPDX header validation and generation scripts

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) provide.io llc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+"""This package contains tests for the tf-wire library."""
+
+# 🥣🔬🔚
