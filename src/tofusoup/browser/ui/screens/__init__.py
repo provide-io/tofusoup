d4b7bc39674226758edafc16a1d907ba2e020393
Tim <code@tim.life>
2026-01-10 20:00:00 -0800

    chore(spdx): add SPDX header validation and generation scripts

---
+++ src/tofusoup/browser/ui/screens/__init__.py
@@ -0,0 +1,8 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) provide.io llc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+"""Screens for the tfbrowser TUI application."""
+
+# 🥣🔬🔚
